1deb999aab12d52613f6047272656605e2a7ffdd
Austin Moen <moenaustin20@gmail.com>
2020-04-20 14:33:21 -0500

    Added superhero model and superhero index

---
+++ Superheroes/Controllers/SuperheroController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Superheroes.Data;
+
+namespace Superheroes.Controllers
+{   
+    public class SuperheroController : Controller
+    {
+        private ApplicationDbContext _context;
+        SuperheroController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
