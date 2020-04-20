aa22bad6c2aaded6ae9328dd9aeecab63c85d437
Austin Moen <moenaustin20@gmail.com>
2020-04-20 16:01:25 -0500

    Added where you can click on names and display detials

--- Superheroes/Controllers/SuperheroController.cs
+++ Superheroes/Controllers/SuperheroController.cs
@@ -4,20 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Superheroes.Data;
+using Superheroes.Models;
 
 namespace Superheroes.Controllers
 {   
     public class SuperheroController : Controller
     {
         private ApplicationDbContext _context;
-        SuperheroController()
+        public SuperheroController(ApplicationDbContext context)
         {
-
+            _context = context;
         }
         public IActionResult Index()
         {
+            var superheroes = _context.Superheroes.ToList();
+            return View(superheroes);
+        }
+        public IActionResult Create()
+        {
             return View();
         }
-
+        
+        [HttpPost]
+        public IActionResult Create(Superhero superhero)
+        {
+            _context.Add(superhero);
+            _context.SaveChanges();
+            return View();
+        }
+        public IActionResult Details(int Id = 0)
+        {
+            Superhero superhero = _context.Superheroes.Find(Id);
+            return View(superhero);
+        }
     }
 }
